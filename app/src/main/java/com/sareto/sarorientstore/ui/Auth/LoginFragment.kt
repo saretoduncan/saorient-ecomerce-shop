2e8b54ba29b7787c2648a5b0427f7056f667b2d7
Duncan <duncan.moiyo@gmail.com>
2022-07-08 21:18:33 +0300

    add firebase data source class

--- app/src/main/java/com/sareto/sarorientstore/ui/Auth/LoginFragment.kt
+++ app/src/main/java/com/sareto/sarorientstore/ui/Auth/LoginFragment.kt
@@ -2,7 +2,6 @@ package com.sareto.sarorientstore.ui.Auth
 
 import android.os.Bundle
 import android.util.Patterns
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
