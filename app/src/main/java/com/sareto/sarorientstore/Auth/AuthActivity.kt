3c09e99d499a7b726f2931530dace4e71535a282
Duncan <duncan.moiyo@gmail.com>
2022-07-08 17:52:24 +0300

    initial commit

---
+++ app/src/main/java/com/sareto/sarorientstore/Auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.sareto.sarorientstore.Auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sareto.sarorientstore.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
---
+++ app/src/main/java/com/sareto/sarorientstore/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sareto.sarorientstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
