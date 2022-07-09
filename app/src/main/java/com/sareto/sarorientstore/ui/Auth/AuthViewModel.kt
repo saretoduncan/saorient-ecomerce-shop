95593ae705cb6aba9aed760384070856259ddb9b
Duncan <duncan.moiyo@gmail.com>
2022-07-09 10:06:56 +0300

    add auth view model

--- app/src/main/java/com/sareto/sarorientstore/ui/Auth/AuthViewModel.kt
+++ app/src/main/java/com/sareto/sarorientstore/ui/Auth/AuthViewModel.kt
@@ -1,4 +1,65 @@
 package com.sareto.sarorientstore.ui.Auth
 
-class AuthViewModel() {
+import androidx.lifecycle.ViewModel
+import com.sareto.sarorientstore.data.repositories.UserRepository
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+class AuthViewModel(private val repository: UserRepository):ViewModel() {
+    // email and password input
+    var email:String?= null
+    var password:String?= null
+
+    //auth listener
+    var authListener:AuthListener?= null
+    //disposable to dispose the  the compatible
+    private val disposables = CompositeDisposable()
+
+    val user by lazy {
+        repository.currentUser()
+    }
+    //login
+    fun login (){
+        if(email.isNullOrBlank()|| password.isNullOrBlank()) {
+            authListener?.onFailure("Invalid Email Or Password")
+            return
+        }
+        authListener?.onStarted()
+        //calling login from repository to perform actual authentication
+        val diposable = repository.login(email!!, password!!)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                //send a success callback
+                authListener?.onSuccess()
+            },{
+                authListener?.onFailure(it.message!!)
+            })
+        disposables.add(disposable)
+    }
+    //signup
+    fun signup(){
+        if(email.isNullOrBlank() || password.isNullOrBlank()){
+            authListener?.onFailure("please input all values")
+            return
+
+        }
+        authListener?.onStarted()
+        val disposable= repository.signup(email!!, password!!)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                authListener?.onSuccess()
+            },{
+                authListener?.onFailure(it.message!!)
+            })
+        disposables.add(disposable)
+    }
+    //disposing the disposables
+    override fun onCleared() {
+        super.onCleared()
+        disposables.dispose()
+    }
 }
