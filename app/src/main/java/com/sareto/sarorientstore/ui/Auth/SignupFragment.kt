60319479e1c5ab0122acead2a11b296bd68d3324
Duncan <duncan.moiyo@gmail.com>
2022-07-10 23:54:10 +0300

    add navigation from signup to login function

--- app/src/main/java/com/sareto/sarorientstore/ui/Auth/SignupFragment.kt
+++ app/src/main/java/com/sareto/sarorientstore/ui/Auth/SignupFragment.kt
@@ -5,38 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.sareto.sarorientstore.R
+import com.sareto.sarorientstore.databinding.FragmentSignupBinding
+import com.sareto.sarorientstore.ui.base.BaseFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SignupFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SignupFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class SignupFragment : BaseFragment<FragmentSignupBinding>()  {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.signUpLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_signupFragment_to_loginFragment)
+        }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        binding.signupButton.setOnClickListener{
+            signup()//signup button
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false)
+    private fun signup() {
+        TODO("Not yet implemented")
     }
 
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    )= FragmentSignupBinding.inflate(inflater, container, false)
 
 }
