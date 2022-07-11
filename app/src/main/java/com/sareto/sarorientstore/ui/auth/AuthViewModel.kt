228ea11a6658075b58b8d8245754173e6b5aeb33
Duncan <duncan.moiyo@gmail.com>
2022-07-11 14:55:29 +0300

    add user authentication view model

--- app/src/main/java/com/sareto/sarorientstore/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/sareto/sarorientstore/ui/auth/AuthViewModel.kt
@@ -1,64 +1,45 @@
-//package com.sareto.sarorientstore.ui.auth
-//
-//import androidx.lifecycle.ViewModel
-//import com.sareto.sarorientstore.data.repositories.UserRepository
-//import io.reactivex.android.schedulers.AndroidSchedulers
-//import io.reactivex.disposables.CompositeDisposable
-//import io.reactivex.schedulers.Schedulers
-//
-//class AuthViewModel(private val repository: UserRepository):ViewModel() {
-//    // email and password input
-//    var email:String?= null
-//    var password:String?= null
-//
-//    //auth listener
-//    var authListener:AuthListener?= null
-//    //disposable to dispose the  the compatible
-//    private val disposables = CompositeDisposable()
-//
-//    val user by lazy {
-//        repository.currentUser()
-//    }
-//    //login
-//    fun login (){
-//        if(email.isNullOrBlank()|| password.isNullOrBlank()) {
-//            authListener?.onFailure("Invalid Email Or Password")
-//            return
-//        }
-//        authListener?.onStarted()
-//        //calling login from repository to perform actual authentication
-//        val disposable = repository.login(email!!, password!!)
-//            .subscribeOn(Schedulers.io())
-//            .observeOn(AndroidSchedulers.mainThread())
-//            .subscribe({
-//                //send a success callback
-//                authListener?.onSuccess()
-//            },{
-//                authListener?.onFailure(it.message!!)
-//            })
-//        disposables.add(disposable)
-//    }
-//    //signup
-//    fun signup(){
-//        if(email.isNullOrBlank() || password.isNullOrBlank()){
-//            authListener?.onFailure("please input all values")
-//            return
-//
-//        }
-//        authListener?.onStarted()
-//        val disposable= repository.signup(email!!, password!!)
-//            .subscribeOn(Schedulers.io())
-//            .observeOn(AndroidSchedulers.mainThread())
-//            .subscribe({
-//                authListener?.onSuccess()
-//            },{
-//                authListener?.onFailure(it.message!!)
-//            })
-//        disposables.add(disposable)
-//    }
-//    //disposing the disposables
-//    override fun onCleared() {
-//        super.onCleared()
-//        disposables.dispose()
-//    }
-//}
+package com.sareto.sarorientstore.ui.auth
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.AuthResult
+import com.sareto.sarorientstore.data.repositories.UserRepository
+import com.sareto.sarorientstore.data.utility.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+class AuthViewModel: ViewModel() {
+    private val _userRegistrationStatus = MutableLiveData<Resource<AuthResult>>()
+    val userRegistrationStatus: LiveData<Resource<AuthResult>> = _userRegistrationStatus
+    private val _userSignInStatus = MutableLiveData<Resource<AuthResult>>()
+    val userSignInStatus: LiveData<Resource<AuthResult>> = _userSignInStatus
+    private val userRepo = UserRepository()
+    fun RegisterUser(name:String, email:String, password:String){
+        _userRegistrationStatus.postValue(Resource.Loading())
+        viewModelScope.launch (Dispatchers.Main) {
+           try {
+               val regResult = userRepo.createUser(name,email,password)
+               _userRegistrationStatus.postValue(regResult)
+           }catch (err:Exception){
+              _userRegistrationStatus.postValue(Resource.Error(err.message?:"unknown error"))
+           }
+
+        }
+
+    }
+    fun loginUser(email: String, password: String){
+        _userRegistrationStatus.postValue(Resource.Loading())
+        viewModelScope.launch {
+            try {
+            val loginResults= userRepo.login(email, password)
+                _userSignInStatus.postValue(loginResults)
+            }catch (err:Exception){
+                _userSignInStatus.postValue(Resource.Error(err.message?:"unknown error"))
+            }
+        }
+
+    }
+}
