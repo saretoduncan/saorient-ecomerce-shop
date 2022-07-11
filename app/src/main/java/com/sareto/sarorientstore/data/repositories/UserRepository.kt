04263f8b441e55f6387c1dcaeed0d1ff45fca2ff
Duncan <duncan.moiyo@gmail.com>
2022-07-11 13:54:38 +0300

    add login and createuser suspend funtion

--- app/src/main/java/com/sareto/sarorientstore/data/repositories/UserRepository.kt
+++ app/src/main/java/com/sareto/sarorientstore/data/repositories/UserRepository.kt
@@ -1,10 +1,40 @@
 package com.sareto.sarorientstore.data.repositories
 
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+import com.sareto.sarorientstore.data.models.User
+import com.sareto.sarorientstore.data.utility.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 
 class UserRepository {
    private val firebaseAuth=FirebaseAuth.getInstance();
-//    private val databaseReference = Firebase
+    private val databaseReference = FirebaseDatabase.getInstance().getReference("users")
+
+    suspend fun createUser(name:String,email:String, password:String):Resource<AuthResult>{
+        return try {
+            val registrationResults = firebaseAuth.createUserWithEmailAndPassword(email, password).await()
+            val userId= registrationResults.user?.uid!!
+            val newUser= User(name, email)
+            databaseReference.child(userId).setValue(newUser).await()
+            Resource.Success(registrationResults)
+        }catch (e:Exception){
+            Resource.Error(e.message?:"unKnown error")
+        }
+    }
+    suspend fun login(email: String, password: String):Resource<AuthResult>{
+        return try {
+            val result= firebaseAuth.signInWithEmailAndPassword(email, password).await()
+            Resource.Success(result)
+        }catch (e:Exception){
+            Resource.Error(e.message?:"unKnown error")
+        }
+    }
 
 }
