42331bfdc4b36546e88f73626825cb3351219c95
Duncan <duncan.moiyo@gmail.com>
2022-07-11 15:50:36 +0300

    add validation to signup form

--- app/src/main/java/com/sareto/sarorientstore/ui/auth/SignupFragment.kt
+++ app/src/main/java/com/sareto/sarorientstore/ui/auth/SignupFragment.kt
@@ -1,16 +1,20 @@
 package com.sareto.sarorientstore.ui.auth
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.sareto.sarorientstore.R
 import com.sareto.sarorientstore.databinding.FragmentSignupBinding
 import com.sareto.sarorientstore.ui.base.BaseFragment
+import com.sareto.sarorientstore.ui.viewModels.AuthViewModel
 
 
 class SignupFragment : BaseFragment<FragmentSignupBinding>()  {
+    private val authViewModel:AuthViewModel by viewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.signUpLogin.setOnClickListener {
@@ -23,7 +27,27 @@ class SignupFragment : BaseFragment<FragmentSignupBinding>()  {
     }
 
     private fun signup() {
-        TODO("Not yet implemented")
+       val fullNames: String = binding.edFirstName.text.toString().trim()
+        val email = binding.edEmail.text.toString().trim()
+        val password = binding.edSignupPassword.text.toString().trim()
+        val confirmPassword = binding.edConfirmPassword.text.toString().trim()
+        if(fullNames.isEmpty()){
+            binding.edFirstName.error="please enter your name"
+            return
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.edEmail.error="Invalid Email Address"
+            return
+        }
+        if(password.isEmpty()){
+            binding.edSignupPassword.error= "Please enter Your password"
+            return
+        }
+        if(password != confirmPassword){
+            binding.edConfirmPassword.error="password doesn't match"
+            return
+        }
+
     }
 
     override fun getFragmentBinding(
