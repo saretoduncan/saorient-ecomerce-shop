d87ddbbd955ae0898a91196620546c53e201e8df
Duncan <duncan.moiyo@gmail.com>
2022-07-13 19:42:22 +0300

    add get all products end point

--- app/src/main/java/com/sareto/sarorientstore/data/network/apiServices/IProductsService.kt
+++ app/src/main/java/com/sareto/sarorientstore/data/network/apiServices/IProductsService.kt
@@ -1,8 +1,14 @@
 package com.sareto.sarorientstore.data.network.apiServices
 
+import com.sareto.sarorientstore.data.models.Products
+import retrofit2.Response
+import retrofit2.http.GET
 import retrofit2.http.Headers
 
 interface IProductsService {
     @Headers("Content-Type:application/json")
+    @GET("products")//get all products
+    suspend fun getAllProducts():Response<Products>
+
 
 }
