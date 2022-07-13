8ce9825dc3ed0eeb85eb90252aae8b8363991a7f
Duncan <duncan.moiyo@gmail.com>
2022-07-13 19:46:22 +0300

    add get product by id

--- app/src/main/java/com/sareto/sarorientstore/data/network/apiServices/IProductsService.kt
+++ app/src/main/java/com/sareto/sarorientstore/data/network/apiServices/IProductsService.kt
@@ -1,14 +1,19 @@
 package com.sareto.sarorientstore.data.network.apiServices
 
 import com.sareto.sarorientstore.data.models.Products
+import com.sareto.sarorientstore.data.models.ProductsItem
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 
 interface IProductsService {
     @Headers("Content-Type:application/json")
     @GET("products")//get all products
     suspend fun getAllProducts():Response<Products>
+    @GET("products/{productId}")//get product by id
+    suspend fun getProductById(@Path("productId") productId:String):Response<ProductsItem>
+
 
 
 }
