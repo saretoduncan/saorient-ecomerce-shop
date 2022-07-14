5f2a78800cd9f1ff2099c9af256abbc02b8d7a83
Duncan <duncan.moiyo@gmail.com>
2022-07-14 23:09:16 +0300

    add comments to all methods in products helper impl

--- app/src/main/java/com/sareto/sarorientstore/data/network/apiHelpers/impl/ProductsHelperImpl.kt
+++ app/src/main/java/com/sareto/sarorientstore/data/network/apiHelpers/impl/ProductsHelperImpl.kt
@@ -11,10 +11,10 @@ import javax.inject.Inject
 class ProductsHelperImpl
 @Inject
 constructor(private val productsService: IProductsService):IProductsServiceHelper{
-    override suspend fun getAllProducts()= productsService.getAllProducts()
+    override suspend fun getAllProducts()= productsService.getAllProducts()//get all products
 
-    override suspend fun getProductById(id: String)=productsService.getProductById(id)
-    override suspend fun getAllCategories()=productsService.getAllCategories()
+    override suspend fun getProductById(id: String)=productsService.getProductById(id) //get products by id
+    override suspend fun getAllCategories()=productsService.getAllCategories()//get all product categories
 
-    override suspend fun getProductsByCategory(categoryName: String)=productsService.getProductsByCategory(categoryName)
+    override suspend fun getProductsByCategory(categoryName: String)=productsService.getProductsByCategory(categoryName)//get products by category
 }
