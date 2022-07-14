f9135f3940f59d01a6fd5d8c3ceb11a008234207
Duncan <duncan.moiyo@gmail.com>
2022-07-14 23:15:08 +0300

    add all suspend functions to products repository

--- app/src/main/java/com/sareto/sarorientstore/data/repositories/ProductsRepository.kt
+++ app/src/main/java/com/sareto/sarorientstore/data/repositories/ProductsRepository.kt
@@ -1,4 +1,12 @@
 package com.sareto.sarorientstore.data.repositories
 
-class ProductsRepository {
+import com.sareto.sarorientstore.data.network.apiHelpers.impl.ProductsHelperImpl
+import javax.inject.Inject
+
+class ProductsRepository
+@Inject constructor(private val productsHelperImpl: ProductsHelperImpl){
+    suspend fun getAllProducts()=productsHelperImpl.getAllProducts()
+    suspend fun getProductById(id:String)= productsHelperImpl.getProductById(id)
+    suspend fun getAllCategories()= productsHelperImpl.getAllCategories()
+    suspend fun getProductsByCategory(categoryName:String)= productsHelperImpl.getProductsByCategory(categoryName)
 }
