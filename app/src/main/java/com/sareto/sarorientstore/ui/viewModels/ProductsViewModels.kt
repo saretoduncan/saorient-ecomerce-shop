7fbebb1c409238e1592c7cbc5740cd1a8fa8495d
Duncan <duncan.moiyo@gmail.com>
2022-07-15 10:22:47 +0300

    Add get Product by id function to products viewModel

--- app/src/main/java/com/sareto/sarorientstore/ui/viewModels/ProductsViewModels.kt
+++ app/src/main/java/com/sareto/sarorientstore/ui/viewModels/ProductsViewModels.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sareto.sarorientstore.data.models.Products
+import com.sareto.sarorientstore.data.models.ProductsItem
 import com.sareto.sarorientstore.data.repositories.ProductsRepository
 import com.sareto.sarorientstore.data.utility.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -18,6 +19,9 @@ class ProductsViewModels
     private val _productsResponse = MutableLiveData<Resource<Products>>()// get all products response
     val productsResponse:LiveData<Resource<Products>>
     get()= _productsResponse// products response live data
+    private val _productByIdResponse = MutableLiveData<Resource<ProductsItem>>()//get product by id response
+    val productByIdResponse:LiveData<Resource<ProductsItem>>
+    get()=_productByIdResponse//get product by livedata
     init {
         getAllProducts()//get all products
     }
@@ -28,5 +32,12 @@ class ProductsViewModels
                _productsResponse.postValue(Resource.Success(it.body()!!))
            }else _productsResponse.postValue(Resource.Error(it.errorBody().toString(), null))
        }
+    }
+    fun getProductById(id:String)= viewModelScope.launch {
+        _productByIdResponse.postValue(Resource.Loading(null))
+        productsRepository.getProductById(id).let {
+            if(it.isSuccessful) _productByIdResponse.postValue(Resource.Success(it.body()!!))
+            else _productByIdResponse.postValue(Resource.Error(it.errorBody().toString(), null))
+        }
     }
 }
