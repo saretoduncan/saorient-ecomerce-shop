dfed3de504790c99bad2c0c136eef8ff97e60f36
Duncan <duncan.moiyo@gmail.com>
2022-07-18 15:35:31 +0300

    Add move from login to product activity functionality

--- app/src/main/java/com/sareto/sarorientstore/data/utility/Utils.kt
+++ app/src/main/java/com/sareto/sarorientstore/data/utility/Utils.kt
@@ -7,10 +7,11 @@ sealed class Resource<T>(val data: T? = null, val message: String? = null) {
     class Loading<T>(data: T? = null) : Resource<T>(data)
     class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
 }
-inline fun <T> safeCall(action:()->Resource<T>):Resource<T>{
-    return try{
+inline fun <T> safeCall(action:()->Resource<T>):Resource<T> {
+    return try {
         action()
-    }catch(e:Exception){
-        Resource.Error(e.message?: "An unknown Error Occurred")
+    } catch (e: Exception) {
+        Resource.Error(e.message ?: "An unknown Error Occurred")
     }
 }
+
--- app/src/main/java/com/sareto/sarorientstore/ui/auth/SignupFragment.kt
+++ app/src/main/java/com/sareto/sarorientstore/ui/auth/SignupFragment.kt
@@ -1,5 +1,6 @@
 package com.sareto.sarorientstore.ui.auth
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.util.Patterns
@@ -14,6 +15,7 @@ import com.sareto.sarorientstore.R
 import com.sareto.sarorientstore.data.utility.Resource
 import com.sareto.sarorientstore.databinding.FragmentSignupBinding
 import com.sareto.sarorientstore.ui.base.BaseFragment
+import com.sareto.sarorientstore.ui.products.ProductsMainActivity
 import com.sareto.sarorientstore.ui.viewModels.AuthViewModel
 
 
@@ -58,7 +60,8 @@ class SignupFragment : BaseFragment<FragmentSignupBinding>()  {
                     Log.d("login......","loading")
                 }
                 is Resource.Success ->{
-                    Toast.makeText(activity,"success", Toast.LENGTH_LONG).show()
+                    val intent = Intent(requireActivity(), ProductsMainActivity::class.java)
+                    startActivity(intent)
                 }
                 is Resource.Error -> {
                     Toast.makeText(activity, it.message, Toast.LENGTH_LONG).show()
